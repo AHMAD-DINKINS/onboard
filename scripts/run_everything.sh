0bcfbdb5a4acde91be17862250d5a1fa6b26c9f3
AHMAD-DINKINS <ahmadid2@illinois.edu>
2021-06-10 19:18:47 -0500

    initial bugs fixed

--- scripts/run_everything.sh
+++ scripts/run_everything.sh
@@ -1,50 +1,49 @@
 #!/bin/sh
 
-# # clean repo
-# mvn clean
-# mvn compile
+# clean repo
+mvn clean
+mvn compile
 
-# rm ./src/test/java/*
+rm ./src/test/java/*
 
-# #run test gens
-# mvn evosuite:generate -DcutsFile=myclasses.txt -DnumberOfCores=2 -DmemoryInMB=1600 -DtimeInMinutesPerClass=4 evosuite:export
+#run test gens
+mvn evosuite:generate -DcutsFile=myclasses.txt -DnumberOfCores=2 -DmemoryInMB=1600 -DtimeInMinutesPerClass=4 evosuite:export
 
-# #assuming path to dependencies CHANGE THIS LATER
-# java -cp ./target/classes:../randoop-4.2.6/randoop-all-4.2.6.jar randoop.main.Main gentests --classlist=myclasses.txt --junit-output-dir=./src/test/java --time-limit=240
+#assuming path to dependencies CHANGE THIS LATER
+java -cp ./target/classes:./dependencies/randoop-4.2.6/randoop-all-4.2.6.jar randoop.main.Main gentests --classlist=myclasses.txt --junit-output-dir=./src/test/java --time-limit=240
 
 # # comment try catch
-# for file in ./src/test/java/*
-#   do python ./scripts/comment_try_catch.py --test $file
-# done
+for file in ./src/test/java/*
+  do python ./scripts/comment_try_catch.py --test $file
+done
 
-# # run tests to generate report
-# mvn test
+# run tests to generate report
+mvn test
 
-# # uncomment try catch
-# for file in ./src/test/java/*
-#   do python ./scripts/comment_try_catch.py --test $file --uncomment
-# done
+# uncomment try catch
+for file in ./src/test/java/*
+  do python ./scripts/comment_try_catch.py --test $file --uncomment
+done
 
-# python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/AppendPairProgram_ESTest.java --report ./target/surefire-reports/TEST-AppendPairProgram_ESTest.xml --return-passing
+python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/AppendPairProgram_ESTest.java --report ./target/surefire-reports/TEST-AppendPairProgram_ESTest.xml --return-passing
 
-# python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/ErrorTest.java --report ./target/surefire-reports/TEST-ErrorTest.xml
+python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/ErrorTest.java --report ./target/surefire-reports/TEST-ErrorTest.xml
 
-# python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/RegressionTest.java --report ./target/surefire-reports/TEST-RegressionTest.xml --return-passing
+python3 scripts/init_test_runner.py --path-to-tests ./src/test/java/RegressionTest.java --report ./target/surefire-reports/TEST-RegressionTest.xml --return-passing
 
-# mvn test-compile
+#TODO comment out assert
 
-#Assuming 
-java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:../junit-4.13.jar daikon.DynComp ErrorTest0
+mvn test-compile
 
-java -cp myclasspath:$DAIKONDIR/daikon.jar daikon.Chicory --comparability-file=ErrorTest0.decls-DynComp ErrorTest0
+# #Assuming and add loop for multiple files
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:dependencies/junit-4.13.jar daikon.DynComp ErrorTest0
 
-java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:../junit-4.13.jar daikon.DynComp RegressionTest0
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar daikon.Chicory --comparability-file=ErrorTest0.decls-DynComp ErrorTest0
 
-java -cp myclasspath:$DAIKONDIR/daikon.jar daikon.Chicory --comparability-file=RegressionTest0.decls-DynComp RegressionTest0
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:dependencies/junit-4.13.jar daikon.DynComp RegressionTest0
 
-java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:../junit-4.13.jar:../evosuite/standalone_runtime/target/evosuite-standalone-runtime-1.1.1-SNAPSHOT.jar:../hamcrest-core-1.3.jar daikon.DynComp AppenPairProgram_ESTest
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar daikon.Chicory --comparability-file=RegressionTest0.decls-DynComp RegressionTest0
 
-java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:../junit-4.13.jar:../evosuite/standalone_runtime/target/evosuite-standalone-runtime-1.1.1-SNAPSHOT.jar:../hamcrest-core-1.3.jar daikon.Chicory --comparability-file=AppenPairProgram_ESTest.decls-DynComp AppenPairProgram_ESTest
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:dependencies/junit-4.13.jar:dependencies/evosuite/standalone_runtime/target/evosuite-standalone-runtime-1.1.1-SNAPSHOT.jar:dependencies/hamcrest-core-1.3.jar daikon.DynComp AppenPairProgram_ESTest
 
-
-
+java -cp ./target/classes:./target/test-classes:$DAIKONDIR/daikon.jar:dependencies/junit-4.13.jar:dependencies/evosuite/standalone_runtime/target/evosuite-standalone-runtime-1.1.1-SNAPSHOT.jar:dependencies/hamcrest-core-1.3.jar daikon.Chicory --comparability-file=AppenPairProgram_ESTest.decls-DynComp AppenPairProgram_ESTest
