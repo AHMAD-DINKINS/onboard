93e939489b3bccca2965f7a5e8a4f8420b4ae5d9
AHMAD-DINKINS <ahmadid2@illinois.edu>
2021-07-03 22:02:00 -0500

    renaming script that inserts mains and changing implementation

---
+++ scripts/insert_mains.py
@@ -0,0 +1,174 @@
+import argparse
+import json
+import os
+import textwrap
+from types import new_class
+import xml.etree.ElementTree as ET
+import random
+import re
+
+
+def get_passing_failing_tests(report):
+    # ignoring asseriton errors in regresssion tests
+    tree = ET.parse(report)
+    root = tree.getroot()
+    tests = {"PASSING":[]}
+
+    tests_tree = root.findall('testcase')
+    for child in tests_tree:
+        failure = child.find('failure')
+        error = child.find('error')
+        class_name = child.get('classname')
+        if child.find('skipped') is not None:
+            continue
+        elif failure is not None:
+            # if not 'AssumptionViolated' in failure.get('type'):
+            typ = failure.get('type').replace(":", "")
+            lpos_of_dot = typ.rindex('.')
+            typ = typ[lpos_of_dot + 1:]
+            if typ == "AssertionError" and "Regression" in class_name:
+                continue
+            if typ in tests:
+                tests[typ].append((child.get('name'), class_name))
+            else:
+                tests[typ] = [(child.get('name'), class_name)]
+        elif error is not None:
+            # if not 'AssumptionViolated' in error.get('type')[:-1]:
+            typ = error.get('type').replace(":", "")
+            lpos_of_dot = typ.rindex('.')
+            typ = typ[lpos_of_dot + 1:]
+            if typ in tests:
+                tests[typ].append((child.get('name'), child.get('classname')))
+            else:
+                tests[typ] = [(child.get('name'), child.get('classname'))]
+        # Double check test is passing one
+        elif not "Error" in child.get('name') and not "Exception" in child.get('name'):
+            tests["PASSING"].append((child.get('name'), child.get('classname')))
+
+    return tests
+
+    
+def create_main(tests, path_to_tests, keeping):
+
+    
+    for test_type in tests:
+        test_list = tests[test_type]
+
+        if len(test_list) == 0: continue
+
+        test_class, selected = get_random_tests(test_list, keeping, test_type)
+        
+        test_filenames = " ".join(path_to_tests)
+        
+        end = test_class[-1]
+        #TODO fix this
+        if end.isdigit():
+            test_class = test_class[:len(test_class) - 1]
+        if not test_class in test_filenames:
+            continue
+        pattern = ' ?(.*' + test_class + '.*) ?'
+        pattern = pattern.replace("\\S", "\S")
+        match = re.match(pattern, test_filenames).group(0)
+        if end.isdigit():
+            match = match.replace(test_class, test_class + end)
+            test_class = test_class + end
+        path_to_testfile = os.path.abspath(match)
+
+        with open(path_to_testfile) as f:
+            lines = f.readlines()
+
+        for line_idx in range(len(lines)):
+            if "class" in lines[line_idx] and test_class in lines[line_idx]:
+                lines[line_idx] += """
+
+public static void main(String[] args) throws Throwable {
+""" + "();\n".join(selected) + """();
+}
+"""
+                break     
+
+        new_file = check_call_PUT(lines, tests, "TestStudentSubmission")
+
+
+
+        new_file = new_file.replace("public void test", "public static void test")
+        lpos_of_fslash = path_to_testfile.rindex("/")
+        class_name = path_to_testfile[lpos_of_fslash + 1:].replace(".java", "")
+        new_class_name = class_name + "_" + test_type
+        new_file_name = path_to_testfile.replace(".java", "_" + test_type + ".java")
+        with open(new_file_name, 'w') as f:
+            new_file = new_file.replace(class_name + " ", new_class_name + " ")
+            f.write(new_file)
+
+
+def get_random_tests(tests, keeping, test_type):
+    test_classes = {}
+    for test in tests:
+        if test[1] in test_classes:
+            test_classes[test[1]].append(test[0])
+        else:
+            test_classes[test[1]] = [test[0]]
+    
+    test_classes_keys = list(test_classes.keys())
+    test_class = test_classes_keys[random.randint(0, len(test_classes_keys) - 1)]
+
+    tests = test_classes[test_class]
+
+    if len(tests) <= keeping or "AssertionError" == test_type: return test_class, tests
+    selected = []
+    while len(selected) < keeping:
+        test_pos = random.randint(0, len(tests) - 1)
+        test = tests[test_pos]
+        tests.remove(test)
+        selected.append(test)
+    return test_class, selected
+
+
+def check_call_PUT(f_lines, tests, PUT_name):
+    remove = []
+    for test in tests:
+        start = pos_of_test(test[0], f_lines)
+        found = False
+        for line_idx in range(start, len(f_lines)):
+            if PUT_name in f_lines[line_idx]:
+                found = True
+            if found or "@Test" in f_lines[line_idx]:
+                break
+        if not found:
+            remove.append(test[0] + "();")
+
+    f_lines = "".join(f_lines)
+
+    for elem in remove:
+        f_lines = f_lines.replace(elem, "")
+    
+    return f_lines
+
+def pos_of_test(test, lines):
+    idx = -1
+    for line_idx in range(len(lines)):
+        if "void " + test in lines[line_idx]:
+            idx = line_idx
+            break
+    return idx
+
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog="initTestRunner")
+    parser.add_argument("--path-to-tests", help="path of test(s) to instrument", type=str, nargs='+', required=True)
+    parser.add_argument("--report", help="xml report with information on test", type=str, required=True)
+    # parser.add_argument("--return-passing", help="Return passing tests cases (default is false)", default=False, action='store_true')
+    parser.add_argument("-k", "--keeping", help="The amount of test to write in each main", type=int, default=3)
+
+    args = parser.parse_args()
+
+    path_to_tests = args.path_to_tests
+    report = args.report
+    # return_passing = args.return_passing
+    keeping = args.keeping
+
+    tests = get_passing_failing_tests(report)
+
+    create_main(tests, path_to_tests, keeping)
+
