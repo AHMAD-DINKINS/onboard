f1aa053f5b1efd6d9e1aae175591d13a743df714
Angello Astorga <aastorg2@illinois.edu>
2021-07-07 12:12:17 -0500

    updating jqf pair program

--- src/main/java/PairProgramJQF.java
+++ src/main/java/PairProgramJQF.java
@@ -11,20 +11,35 @@ import static org.junit.Assume.assumeTrue;
 @RunWith(JQF.class)
 public class PairProgramJQF {
 
-  // public int OldCount;
-  // public boolean OldIsLengthOdd;
+  public int OldCount;
+  public int OldHead;
+  public int OldElem;
+  public boolean OldIsLengthOdd;
+  public boolean OldIsValueAtEnd;
+  
+  public boolean safe= false;
+
   @Fuzz
   public void TestStudentSubmission(@From(ListGenerator.class) List l, int elem) 
   {
-    assumeTrue(l != null);
-    // OldCount = l.size();
-    // OldIsLengthOdd = l.isLengthOdd();
-    assumeTrue(l != null && l.next != l);
+    safe = false;
+    assumeTrue(l != null && !l.isCyclic());
+    OldCount = l.size();
+    OldHead = l.value;
+    OldElem = elem;
+    OldIsLengthOdd = l.isLengthOdd();
+    OldIsValueAtEnd = l.isValueAtEnd(elem);
+    assumeTrue(true);
     List copy = clone(l);
+    
     l.addToEndRef(elem);
     copy.addToEndStu(elem);
 
-    assertTrue(IsEqual(l, copy));
+    boolean isEqual = IsEqual(l,copy);
+    if (isEqual)
+      safe = true;
+    
+    assertTrue(isEqual);
   }
 
   // Generates all possible valid lists
@@ -42,6 +57,7 @@ public class PairProgramJQF {
   private static List clone(List l) {
     assumeTrue(l.next != l);
     List other = new List();
+    List head = other;
     List temp = l;
     for (; temp.next != null; temp = temp.next) {
       other.value = temp.value;
@@ -49,7 +65,7 @@ public class PairProgramJQF {
       other = other.next;
     }
     other.value = temp.value;
-    return other;
+    return head;
   }
 
   private boolean IsEqual(List R, List C) {
