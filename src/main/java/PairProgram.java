0a985864c6b534872c66144af8470c84832d813c
AHMAD-DINKINS <ahmadid2@illinois.edu>
2021-07-10 15:41:38 -0500

    pair program for sum

--- src/main/java/PairProgram.java
+++ src/main/java/PairProgram.java
@@ -10,24 +10,22 @@ import org.junit.runner.RunWith;
 public class PairProgram {
   /*Features*/
   public int OldCount;
-  public int OldHead;
-  public int OldElem;
+  public boolean OldIsAllZeros;
+  public boolean OldLastElemIsZero;
   public boolean OldIsLengthOdd;
-  public boolean OldIsValueAtEnd;
+  public boolean OldIndiciesMatchElements;
   public boolean safe = false;
 
-  public void TestStudentSubmission(List l, int elem)
+  public void TestStudentSubmission(int[] array)
   {
-    assumeTrue(l != null && !l.isCyclic());
-    assumeTrue(false);
-    List copy = clone(l);
+    assumeTrue(array != null);
+    assumeTrue(true);
+    int[] copy = clone(array);
 
-    l.addToEndRef(elem);
-    copy.addToEndStu(elem);
+    int refSum = sumRef(array);
+    int stuSum = sumStu(copy);
     
-    //assumeTrue(IsEqual(l, copy));
-    
-    boolean isEqual = !copy.isCyclic() && IsEqual(l,copy);
+    boolean isEqual = refSum == stuSum && IsEqual(array, copy);
     if (isEqual)
       safe = true;
     
@@ -35,34 +33,86 @@ public class PairProgram {
     
   }
 
-  // Generates all possible valid lists
-  public static List listgen(int[] init) {
-    assumeTrue(init != null);
-    List l = new List();
-    if (init.length >= 1) {
-      l.value = init[0];
+  private static int[] clone(int[] arr) {
+    int[] copy = new int[arr.length];
+    for (int i = 0; i < arr.length; ++i) {
+      copy[i] = arr[i];
     }
-    for (int i = 1; i < init.length; i++)
-      l.addToEndRef(init[i]);
-    return l;
+    return copy;
   }
 
-  private static List clone(List l) {
-    assumeTrue(l.next != l);
-    List other = new List();
-    List head = other;
-    List temp = l;
-    for (; temp.next != null; temp = temp.next) {
-      other.value = temp.value;
-      other.next = new List();
-      other = other.next;
+  public static int[] generateArray(int[] array, int kind) {
+      int[] toRet = new int[array.length];
+      boolean empty = toRet.length == 0;
+      if (empty) {
+        return toRet;
+      }
+      switch (kind) {
+        case 0:
+          // generate all zeros array
+          return toRet;
+        case 1:
+          // generate indiciesMatchElems array
+          for (int i = 0; i < toRet.length; ++i) {
+            toRet[i] = i;
+          }
+          return toRet;
+        case 2:
+          // generate last elem is zero
+          array[array.length - 1] = 0;
+          return array;
+      }
+      return array;
+  }
+
+/**
+ * Return the sum of the elements in array.
+ */
+private static int sumStu(final int[] array) {
+    int count = 0;
+    for (int i = 0; i < array.length; i++) {
+        count++;
     }
-    other.value = temp.value;
-    return head;
-  }
-
-  private boolean IsEqual(List R, List C) {
-    return R.equals(C);
-  }
-
+    return count; 
 }
+private static int sumRef(final int[] array) {
+    int sum = 0;
+    for (int i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    
+    return sum;
+}
+
+private boolean IsEqual(int[] R, int[] C) {
+    if (R.length != C.length) return false;
+    for (int i = 0; i < R.length; ++i) {
+      if (R[i] != C[i]) return false;
+    }
+    return true;
+  }
+
+  /**
+   * Beginning of Obsevers
+   */
+  private static boolean isAllZeros(int[] arr) {
+    for (int elem: arr) {
+      if (elem != 0) return false;
+    }
+    return true;
+  }
+
+  private static boolean lastElemIsZero(int[] arr) {
+    return arr[arr.length - 1] == 0;
+  }
+
+  private static boolean indiciesMatchElements(int[] arr) {
+    for (int i = 0; i < arr.length; ++i) {
+      if (arr[i] != i) return false;
+    }
+    return true;
+  }
+
+  private static boolean isLengthOdd(int[] arr) {
+    return arr.length % 2 == 1;
+  }}
